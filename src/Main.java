40869d0a8b65ab45424710c0fd9550551b3457c5
Irina Khvatova <i.chwatowa@mail.ru>
2022-07-04 22:44:12 +0300

    File Main.java commited

--- src/Main.java
+++ src/Main.java
@@ -4,42 +4,22 @@ public class Main {
 
         int initAccount = 400; // Сумма в рублях на счете клиента изначально
 
-        int refillAcc1 = 990; // Клиент пополнил счет на сумму, меньшую 1000 рублей
-        int refillAcc2 = 1300; // Клиент пополнил счет на сумму, превышающую 1000 рублей
+        int refillAcc = 1400; // Сумма пополнения счета клиентом
 
 
-        int bonus1 = (refillAcc1 / 100); // Количество бонусных рублей в первом сценарии
-        int bonus2 = (refillAcc2 / 100); // Количество бонусных рублей во втором сценарии
+        int bonus = (refillAcc / 100); // Количество бонусных рублей
 
-        if (refillAcc1 >= 1000) {
-            System.out.println("Первый сценарий:");
+        if (refillAcc >= 1000) {
             System.out.println("Количество бонусных рублей:");
-            System.out.println(bonus1);
+            System.out.println(bonus);
             System.out.println("Итоговая сумма на счете клиента:");
-            System.out.println(initAccount + refillAcc1 + bonus1);
+            System.out.println(initAccount + refillAcc + bonus);
 
         } else {
-            System.out.println("Первый сценарий:");
             System.out.println("Количество бонусных рублей:");
             System.out.println(0);
             System.out.println("Итоговая сумма на счете клиента:");
-            System.out.println(initAccount + refillAcc1);
-        }
-            System.out.println();
-
-        if (refillAcc2 >= 1000) {
-            System.out.println("Второй сценарий:");
-            System.out.println("Количество бонусных рублей:");
-            System.out.println(bonus2);
-            System.out.println("Итоговая сумма на счете клиента:");
-            System.out.println(initAccount + refillAcc2 + bonus2);
-
-        } else {
-            System.out.println("Второй сценарий:");
-            System.out.println("Количество бонусных рублей:");
-            System.out.println(0);
-            System.out.println("Итоговая сумма на счете клиента:");
-            System.out.println(initAccount + refillAcc2);
+            System.out.println(initAccount + refillAcc);
         }
 
     }
